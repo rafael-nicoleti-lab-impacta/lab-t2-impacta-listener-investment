622593cf101792b534e89fb85f75ce37953c5165
Rafael Nicoleti <rafaelnicolett@outlook.com>
2022-02-23 10:12:37 -0300

    First Commit

---
+++ consumer-investiment-done/src/main/java/br/com/lab/consumerinvestimentdone/ConsumerInvestmentDoneApplication.java
@@ -0,0 +1,13 @@
+package br.com.lab.consumerinvestimentdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerInvestmentDoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerInvestmentDoneApplication.class, args);
+	}
+
+}
---
+++ consumer-investiment-done/src/test/java/br/com/lab/consumerinvestimentdone/ConsumerInvestimentDoneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lab.consumerinvestimentdone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerInvestimentDoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
